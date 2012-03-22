76c4670aded0bc7f4aaea375615a64360f2c827d
Keith Pitty <keith@keithpitty.com>
2012-03-22 23:40:32 +1100

    Generated refinery app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whinkingdrisky::Application.config.secret_token = '36a2fb5886608cd582d382740fd774188cb5310b3032d426fb91ee51d81231657268586d14e49a0992667188cb962d91a51544060e0f57a320d55b059e6b8cc6'
